3617139b4959ef815fe7dffd2dcad91a07224d37
rrooman <pos22_23_git@gmx.at>
2022-10-07 18:17:57 +0200

    Properties

---
+++ Properties/Lehrer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Lehrer
+    {
+        private decimal? _bruttoGehalt;
+
+        public string? Zuname { get; set; }
+        public string Vorname { set; get; } = string.Empty;
+
+        public decimal? Bruttogehalt
+        {
+            get { return _bruttoGehalt; }
+            set { _bruttoGehalt ??= value; }
+        }
+        public string? Kuerzel { 
+            get { return Zuname?[..3]?.ToUpper() ?? ""; }
+        }
+
+        public decimal? Nettogehalt { get 
+            { return (_bruttoGehalt ?? 0) *0.8M; }
+        }
+    }
+}
---
+++ Properties/Rechteck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Rechteck
+    {
+        private int _laenge, _breite;
+
+        public int Laenge
+        {
+            get { return _laenge; }
+            set { _laenge = value > 0 ? value : throw new ArgumentException("Ungültige Länge"); }
+        }
+
+        public int Breite
+        {
+            get { return _breite; }
+            set { _breite = value > 0 ? value : throw new ArgumentException("Ungültige Breite"); }
+        }
+
+        public int Flaeche => _laenge * _breite;
+    }
+}
